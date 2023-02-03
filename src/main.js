1bd54b013432f6deb105c6914043a99b05e752f5
Sebastian <s.korzeniak@gmail.com>
2023-02-03 12:20:54 +0100

    build

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import 'flowbite'
-import 'remixicon/fonts/remixicon.css'
-import vueScrollto from 'vue-scrollto'
-
-
-createApp(App).use(vueScrollto).mount('#app')
